5364271b0be09a399e8a7ffe587d7f4a521b1a44
Adrian <adrberia@gmail.com>
2024-06-13 23:01:50 -0400

    Removed unused test

--- src/app.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import App from "./app.tsx";
-
-describe("<App />", () => {
-  it("should render", () => {
-    const { getByText } = render(<App />);
-    expect(getByText("React App")).toBeInTheDocument();
-  });
-});
